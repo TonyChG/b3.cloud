9126b80938b7d1a7578ccafa503890ed97357c01
TonyChG <antoine.chiny@ynov.com>
2018-11-03 18:34:28 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,31 +10,36 @@
 # =============================================================================
 
 baseuser="core"
+vagrant_ssh_key="$HOME/.vagrant.d/insecure_private_key"
 master_manager="192.168.4.101"
 other_managers=( \
 '192.168.4.102'  \
+'192.168.4.103'  \
 '192.168.4.201'  \
 '192.168.4.202'  \
+'192.168.4.203'  \
 )
 workers=(       \
-'192.168.4.103' \
 '192.168.4.104' \
 '192.168.4.105' \
-'192.168.4.203' \
 '192.168.4.204' \
 '192.168.4.205' \
 )
-# ssh -i $HOME/.vagrant.d/insecure_private_key $baseuser@$master_manager "docker swarm init --advertise-addr master_manager"
-token_manager="$(ssh -o "StrictHostKeyChecking=false" -i $HOME/.vagrant.d/insecure_private_key $baseuser@$master_manager "docker swarm join-token manager" | egrep token | awk '{print$5}')"
-token_worker="$(ssh -o "StrictHostKeyChecking=false" -i $HOME/.vagrant.d/insecure_private_key $baseuser@$master_manager "docker swarm join-token worker" | egrep token | awk '{print$5}')"
+
+ssh -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm leave --force"
+ssh -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm init --advertise-addr $master_manager"
+token_manager="$(ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm join-token manager" | egrep token | awk '{print$5}')"
+token_worker="$(ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm join-token worker" | egrep token | awk '{print$5}')"
 
 echo "Manager Token : $token_manager"
 echo "Worker Token  : $token_worker"
 
 for m in ${other_managers[@]}; do
-    ssh -o "StrictHostKeyChecking=false" -i $HOME/.vagrant.d/insecure_private_key $baseuser@$m "docker swarm join --token $token_manager $master_manager:2377"
+    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$m "docker swarm leave --force"
+    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$m "docker swarm join --token $token_manager $master_manager:2377"
 done
 
 for w in ${workers[@]}; do
-    ssh -o "StrictHostKeyChecking=false" -i $HOME/.vagrant.d/insecure_private_key $baseuser@$w "docker swarm join --token $token_worker $master_manager:2377"
+    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$w "docker swarm leave --force"
+    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$w "docker swarm join --token $token_worker $master_manager:2377"
 done
