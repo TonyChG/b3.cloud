a3b53490b645c4c051ad82f2e92ac5b59f7e3ca7
TonyChG <antoine.chiny@ynov.com>
2018-11-05 09:16:42 +0100

    Add ceph deploy

--- scripts/create_data_volume.sh
+++ scripts/create_data_volume.sh
@@ -10,7 +10,9 @@
 # =============================================================================
 
 vg_name="data"
-lv_name="root"
+lv_name1="docker"
+lv_name2="shared"
+
 target_device=$(/usr/sbin/fdisk -l | grep '10 GiB' | awk -F: '{print $1}' | sed -e 's#Disk ##g')
 
 if [[ -z $target_device ]]; then
@@ -25,15 +27,17 @@ systemctl start lvm2-lvmetad.service
 systemctl start lvm2-lvmetad.socket
 systemctl stop docker
 
-mkdir -p /$vg_name
+mkdir -p /data/{$lv_name1,$lv_name2}
 /usr/sbin/pvcreate $target_device
 /usr/sbin/vgcreate $vg_name $target_device
-/usr/sbin/lvcreate -l 100%FREE -n $lv_name $vg_name
->/dev/null /usr/sbin/mkfs.ext4 "/dev/mapper/$vg_name-$lv_name"
+/usr/sbin/lvcreate -l 50%FREE -n $lv_name1 $vg_name
+/usr/sbin/lvcreate -l 100%FREE -n $lv_name2 $vg_name
+>/dev/null /usr/sbin/mkfs.ext4 "/dev/mapper/$vg_name-$lv_name1"
 
-mount "/dev/mapper/$vg_name-$lv_name" "/$vg_name"
+echo -e "/dev/mapper/$vg_name-$lv_name1\t/data/$lv_name1\text4\tdefaults\t1 1" > /etc/fstab
+mount -a
 
-rm -rf /data/*
+rm -rf /data/{$lv_name1,lv_name2}/*
 chown -R core:core /data
 
 systemctl enable docker
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,35 +11,38 @@
 
 baseuser="core"
 vagrant_ssh_key="$HOME/.vagrant.d/insecure_private_key"
+
 master_manager="192.168.4.101"
 other_managers=( \
 '192.168.4.102'  \
 '192.168.4.103'  \
-'192.168.4.201'  \
-'192.168.4.202'  \
-'192.168.4.203'  \
+# '192.168.4.201'  \
+# '192.168.4.202'  \
+# '192.168.4.203'  \
 )
 workers=(       \
 '192.168.4.104' \
 '192.168.4.105' \
-'192.168.4.204' \
-'192.168.4.205' \
+# '192.168.4.204' \
+# '192.168.4.205' \
 )
 
-ssh -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm leave --force"
-ssh -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm init --advertise-addr $master_manager"
-token_manager="$(ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm join-token manager" | egrep token | awk '{print$5}')"
-token_worker="$(ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$master_manager "docker swarm join-token worker" | egrep token | awk '{print$5}')"
+sshcmd=(ssh -q -o "StrictHostKeyChecking=no" -i $vagrant_ssh_key)
+
+${sshcmd[@]} $baseuser@$master_manager "docker swarm leave --force"
+>/dev/null ${sshcmd[@]} $baseuser@$master_manager "docker swarm init --advertise-addr $master_manager"
+token_manager="$(${sshcmd[@]} $baseuser@$master_manager "docker swarm join-token manager" | egrep token | awk '{print$5}')"
+token_worker="$(${sshcmd[@]} $baseuser@$master_manager "docker swarm join-token worker" | egrep token | awk '{print$5}')"
 
 echo "Manager Token : $token_manager"
 echo "Worker Token  : $token_worker"
 
 for m in ${other_managers[@]}; do
-    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$m "docker swarm leave --force"
-    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$m "docker swarm join --token $token_manager $master_manager:2377"
+    ${sshcmd[@]} $baseuser@$m "docker swarm leave --force"
+    ${sshcmd[@]} $baseuser@$m "docker swarm join --token $token_manager $master_manager:2377"
 done
 
 for w in ${workers[@]}; do
-    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$w "docker swarm leave --force"
-    ssh -o "StrictHostKeyChecking=false" -i $vagrant_ssh_key $baseuser@$w "docker swarm join --token $token_worker $master_manager:2377"
+    ${sshcmd[@]} $baseuser@$w "docker swarm leave --force"
+    ${sshcmd[@]} $baseuser@$w "docker swarm join --token $token_worker $master_manager:2377"
 done
